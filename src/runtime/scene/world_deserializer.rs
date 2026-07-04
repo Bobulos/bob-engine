b41e1c020fe18fccea5353ee242129d565dd5f03
Bobulos <142840991+Bobulos@users.noreply.github.com>
2026-07-04 16:20:33 -0700

    add untracked

---
+++ src/runtime/scene/world_deserializer.rs
@@ -0,0 +1,5 @@
+use crate::runtime::scene::serialized_world::SerializedWorld;
+pub fn load_to_serialized_world() -> Option<SerializedWorld> {
+    todo!("Figure out wheter to load embedded or not");
+    None
+}
